5354893421ae1dbce827403b8060142648e9bc41
Dariusz <darek200180@gmail.com>
2020-11-04 23:47:32 +0000

    add makeSeatsOccupied function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello world');
+const seats = [...document.querySelectorAll('.row .seat')];
+
+
+makeSeatsOccupied(seats);
+
+function makeSeatsOccupied(seats) {
+    const occupiedSeats = [];
+    seats.forEach((seat, index) => {
+        if(Math.random() > 0.8)
+            occupiedSeats.push(index);
+    });
+
+    console.log(occupiedSeats);
+
+
+    seats.forEach((seat, index) => {
+        seat.classList = 'seat';
+
+        if(occupiedSeats.indexOf(index) > -1)
+            seat.classList = 'seat occupied';
+    })
+}
