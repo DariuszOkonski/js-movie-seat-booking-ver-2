455e189f27bc4f975ee8443602a3162b8aa3b3f0
Dariusz <darek200180@gmail.com>
2020-11-05 00:15:26 +0000

    add funcionality to mark free seat to make them selected

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const seats = [...document.querySelectorAll('.row .seat')];
 const btnReset = document.querySelector('#restartContainer button');
 
+seats.forEach(seat => {
+    seat.addEventListener('click', selectSeat)
+});
 
 makeSeatsOccupied(seats);
+
+function selectSeat(e) {
+    if(e.target.classList[1] !== 'occupied') {
+        console.log('free')
+        e.target.classList.toggle('selected');
+    }
+        
+}
 
 function makeSeatsOccupied(seats) {
     const occupiedSeats = [];
